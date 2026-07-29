8edc255b33bce929b94d5dda80efd704e0f37763
ShivaniGanjai <shivaniganjai10@gmail.com>
2026-07-29 13:30:42 -0400

    hello.py

--- Python basics/hello.py
+++ Python basics/hello.py
@@ -1,2 +1,15 @@
 print("Hello World!")
-print("Welcome")
+print("I am learning Python.")
+print("AWESOME")
+# when a write multiple statements on one line by separating them with ;
+print("How are you"); print("Have a good day"); print("It is awesome")
+print('This will also work!')           #single quotes
+# print multiple words on the same line, using end parameter
+print("Welcome", end=" ")
+print("to", end=" ")
+print("Python")
+# Numbers
+print(3)
+print(5 + 5)
+# combine text and numbers in one output by separating them with a comma
+print("I am", 30, "years old.")
