ddbf4a21ac967c8a5eed10b855664483519ce4e5
ShivaniGanjai <shivaniganjai10@gmail.com>
2026-07-30 13:39:54 -0400

    variables.py

---
+++ Python basics/variables.py
@@ -0,0 +1,69 @@
+name = "John"
+age = 20
+print(name)
+print(age)
+
+a = 10
+b = 30
+print(a,b)
+
+# Valid variable names
+name = "A"
+n_a_m_e = "B"
+_name = "C"
+name2 = "D"
+print(name,n_a_m_e,_name,name2)
+
+x = 5
+x = "Hyderabad"
+print(x)
+
+# Variables are case sensitive
+a = 5
+A = "Java"
+print(a)
+print(A)
+
+subject = "Python"
+marks = 95
+print("Subject:", subject)
+print("Marks:", marks)
+
+year = 2026
+print("Welcome to the year",year)
+
+oranges = 5
+print("I have", oranges, "oranges.")
+
+# Many values to multiple variables
+x, y, z = "Apple", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+# Also use the + operator to output multiple variables
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+# One value to multiple variables
+x = y = z = "Tab"
+print(x)
+print(y)
+print(z)
+
+# Swap two variables
+a = 5
+b = 10
+a, b = b, a
+print(a)
+print(b)
+
+# Take an integer input from the user 
+num = int(input("Enter number: "))
+print(num)
+
+# Take a string (text) input from the user
+name = input("Enter name: ")
+print(name) 
