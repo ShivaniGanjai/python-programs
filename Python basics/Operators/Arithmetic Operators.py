0d4ac91bb1d811a5075ef592d8ca644c82ac0b31
ShivaniGanjai <shivaniganjai10@gmail.com>
2026-08-28 14:02:40 -0400

    Arithmetic Operators.py

---
+++ Python basics/Operators/Arithmetic Operators.py
@@ -0,0 +1,120 @@
+# Addition
+x = 5
+y = 3 
+print(x + y)
+
+# Subtraction
+x = 5 
+y = 3 
+print(x - y)
+
+# Multiplication
+x = 4
+y = 5
+print(x * y)
+
+# Division
+x = 12
+y = 3
+print(x / y)
+
+# Modulus
+x  = 7
+y = 2
+print(x % y)
+
+# Exponentiation
+x = 2
+y = 5
+print(x ** y)
+
+# Floor division
+x = 15
+y = 2
+print(x // y)
+
+# Examples
+x = 15
+y = 4
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+sum1 = 100 + 50
+sum2 = sum1 + 250
+sum3 = sum2 + sum2
+print(sum1)
+print(sum2)
+print(sum3)
+
+# Calculating Total Marks
+a = 80
+b = 75
+c = 90
+total = a + b + c
+print("Total Marks:", total)
+
+# Calculating Average
+a = 10
+b = 20
+c = 30
+average = (a + b + c) / 3
+print("Average:", average)
+
+# Area of circle
+radius = 5
+pi = 3.14
+area = pi * radius ** 2
+print("Area of circle:", area)
+
+# Area of Rectangle 
+length = 10
+breadth = 5
+area = length * breadth 
+print("Area of Rectangle:", area)
+
+# Calculating Simple Interest
+p = 1000
+r = 5
+t = 2
+si = (p * r * t) / 100
+print(si)
+
+# Calculating Percentage
+marks = 450
+total = 500
+percentage = (marks / total) * 100
+print("Percentage:", percentage)
+
+# Examples
+x = 10
+y = 3
+print(x + y * 2)
+print((x + y) * 2)
+
+num = int(input("Enter num: "))
+square = num ** 2
+cube = num ** 3
+print("Square:", square)
+print("cube:", cube)
+
+seconds = 125
+minutes = seconds // 60
+remaining_seconds = seconds % 60
+print("Minutes:", minutes)
+print("Remaining_seconds:", remaining_seconds)
+
+# Simple Calculator
+a = int(input("Enter a value: "))
+b = int(input("Enter b value: "))
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Exponent:", a ** b)
+print("Floor division:", a // b)
+print("Modulus:", a % b)
