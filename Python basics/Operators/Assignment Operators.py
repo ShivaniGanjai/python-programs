ded1e81af7989ebdce5ebba2d28b4a55bdbf3161
ShivaniGanjai <shivaniganjai10@gmail.com>
2026-09-19 12:37:38 -0400

    Assignment Operators.py

---
+++ Python basics/Operators/Assignment Operators.py
@@ -0,0 +1,81 @@
+x = 5
+print(x)
+
+x = 5
+x += 3
+print(x)
+
+x = 5
+x -= 3
+print(x)
+
+x = 5
+x *= 3
+print(x)
+
+x = 5
+x /= 3
+print(x)
+
+x = 5
+x %= 3
+print(x)
+
+x = 5
+x //= 3
+print(x)
+
+x = 5
+x **= 3
+print(x)
+
+x = 5
+x &= 3
+print(x)
+
+x = 5
+x |= 3
+print(x)
+
+x = 5
+x ^= 3
+print(x)
+
+x = 5
+x >>= 3
+print(x)
+
+x = 5
+x <<= 3
+print(x)
+
+print(x := 3)
+
+# Examples
+x = 10
+x += 5
+print(x, type(x))
+x -= 3
+print(x, type(x))
+x *= 2
+print(x, type(x))
+x /= 4
+print(x, type(x))
+x //= 2
+print(x, type(x))
+x **= 3
+print(x, type(x))
+x %= 3
+print(x, type(x))
+
+x = 15
+x |= 3
+print(x, type(x))
+x &= 3
+print(x, type(x))
+x ^= 2
+print(x, type(x))
+x >>= 2
+print(x, type(x))
+x <<= 3
+print(x, type(x))
